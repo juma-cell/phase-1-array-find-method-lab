7420cc432e243598664c06b2065e453e84177076
Juma Musa <juma.musa@moringaschool.com>
2023-03-20 18:36:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(array) {
+    const win = array.find(obj => obj.result === "W");
+    return win ? win.year : undefined;
+  }
+
+
+
+const superbowls = [
+    { year: 2022, result: "L" },
+    { year: 2021, result: "W" },
+    { year: 2020, result: "L" },
+    
+  ];
+  
+  console.log(superbowlWin(superbowls)); 
